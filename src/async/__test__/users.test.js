65fd5a57c3bd38ac4b568e0418d7a1a656153b74
Yafeng Wang <yafeng.wang@wyf.local>
2020-11-14 23:12:18 +0800

    finish test

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,11 @@
+import getUsers from "../users";
+
+jest.mock("axios");
+
 describe("users", () => {
   test("should get users data with mock axios get", () => {
     // TODO 13: add async test with manual mock
+    const data = getUsers();
+    return expect(data).resolves.toEqual({ name: "test" });
   });
 });
--- src/mock.class/__test__/vaccineTest.test.js
+++ src/mock.class/__test__/vaccineTest.test.js
@@ -2,26 +2,51 @@ import VaccineTest from "../vaccineTest";
 import Recipient from "../recipient";
 import Covid19Vaccine from "../covid19Vaccine";
 
+const mockAcceptInjection = jest.fn();
+const mockGetHasAntibodies = jest.fn();
+
 jest.mock("../recipient", () => {
   // mock class实现
+  return jest.fn(() => {
+    return {
+      acceptInjection: mockAcceptInjection,
+      getHasAntibodies: mockGetHasAntibodies,
+    };
+  });
 });
 
 beforeEach(() => {
   // clear mock here
+  Recipient.mockClear();
+  mockAcceptInjection.mockClear();
+  mockGetHasAntibodies.mockClear();
 });
 
 describe("inject", () => {
   test("should recipient accept injection with vaccine", () => {
     // TODO 14: add test here
+    const vaccineTest = new VaccineTest();
+    vaccineTest.inject();
+    expect(mockAcceptInjection).toHaveBeenCalledWith(
+      expect.any(Covid19Vaccine)
+    );
   });
 });
 
 describe("test", () => {
   test("should get Success if recipient has antibodies", () => {
     // TODO 15: add test here
+    const vaccineTest = new VaccineTest();
+    mockGetHasAntibodies.mockReturnValue(true);
+    const ans = vaccineTest.test();
+    expect(ans).toEqual("Vaccine Test Success");
   });
 
   test("should get Failed if recipient has no antibodies", () => {
     // TODO 16: add test here
+    const vaccineTest = new VaccineTest();
+    mockGetHasAntibodies.mockReturnValue(false);
+    const ans = vaccineTest.test();
+    expect(ans).toEqual("Vaccine Test Failed");
   });
 });
